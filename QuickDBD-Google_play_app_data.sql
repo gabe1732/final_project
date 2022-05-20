594f6c171bb1141ae2a0794fa0d38fcf5ca6bd3a
gabe1732 <94803292+gabe1732@users.noreply.github.com>
2022-05-20 16:02:12 -0400

    Add ERD and Data CSV

---
+++ QuickDBD-Google_play_app_data.sql
@@ -0,0 +1,21 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/4Rrkdy
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "googleplaystore" (
+    "app" string   NOT NULL,
+    "category" string   NOT NULL,
+    "rating" int   NOT NULL,
+    "reviews" int   NOT NULL,
+    "size" int   NOT NULL,
+    "installs" int   NOT NULL,
+    "type" string   NOT NULL,
+    "price" int   NOT NULL,
+    "content_rating" string   NOT NULL,
+    "genres" string   NOT NULL,
+    "last_updated" string   NOT NULL,
+    "current_ver" string   NOT NULL,
+    "android_ver" string   NOT NULL
+);
+
