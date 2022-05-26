a60b8139815bb91600da7b1eca5cdda61cf8111e
kkrizek06 <96017493+kkrizek06@users.noreply.github.com>
2022-05-26 19:27:50 -0400

    Add files via upload
    First table

---
+++ TABLE genre_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE genre_data (
+	app_id VARCHAR NOT NULL,
+	app_name VARCHAR NOT NULL,
+    category VARCHAR NOT NULL,
+	app_type VARCHAR NOT NULL,
+	content_rating VARCHAR NOT NULL,
+	genres VARCHAR NOT NULL, 
+	sub_genre INT NOT NULL
+);
